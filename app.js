fa4122634b1d55bb415faed33f2b2be661305ead
Cam205 <cameronbuchanan17@yahoo.com>
2021-09-13 16:43:43 -0500

    Fixed all errors

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 let cells = document.querySelectorAll('.row > div'),
     clickCount = 0
+    let resetButton = document.getElementById('reset');
+let winningCombos = [
+    [cells[0], cells[1], cells[2]],
+    [cells[3], cells[4], cells[5]],
+    [cells[6], cells[7], cells[8]],
+    [cells[0], cells[3], cells[6]],
+    [cells[1], cells[4], cells[7]],
+    [cells[2], cells[5], cells[8]],
+    [cells[0], cells[4], cells[8]],
+    [cells[2], cells[4], cells[6]]
+];
+
+resetButton.addEventListener("click", function () {
+    window.location.reload();
+});
 
 console.log(cells)
 
@@ -7,34 +22,52 @@ for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', cellClicked)
 }
 
-function cellClicked() {
-    event.target.textContent = 'X'
-    console.log("click count: " + clickCount)
+function cellClicked(e) {
+    //event.target.textContent = 'X'
+    //console.log("click count: " + clickCount)
+    //clickCount++
+
+    if (clickCount % 2 == 0) {
+        e.target.textContent = 'X'
+    } else {
+        e.target.textContent = 'O'
+    }
+
     clickCount++
 
-    if (clickCount % 2 == false) {
-        event.target.textContent = 'O'
-    } else if (clickCount >= 9) {
-        alert('We may have a winner')
-    }
+    checkWinner();
 
-    if (cellClicked.textContent == 'X') {
-      checkWinner ()
-      alert("You won X")  
-    }
 }
 
 function checkWinner() {
 
-    let winningCombos = [
-        ['.top left', '.top middle', 'top right'],
-        ['.center left', '.center', '.center right'],
-        ['.bottom left', '.bottom middle', '.bottom right'],
-        ['.top left', '.center middle', '.bottom right'],
-        ['.top right', '.center', '.bottom left']
-        ['.top middle', '.center', '.bottom middle']
-        ['.top left', '.center left', '.bottom left']
-        ['.top right', '.center right', '.bottom right']
-    ]
+    for (i = 0; i < winningCombos.length; i++) {
+        let xCount = 0
+        let oCount = 0
+
+        for (j = 0; j < winningCombos[i].length; j++) {
+
+            if (winningCombos[i][j].textContent == 'X') {
+                xCount++
+            } else if (winningCombos[i][j].textContent == 'O') {
+                oCount++
+            }
+        }
+
+        if (xCount == 3) {
+            alert("You won X")
+            resetButton.style.display = "inline"
+            return
+        } else if (oCount == 3) {
+            alert("You won O")
+            resetButton.style.display = "inline"
+            return
+        }
+
+    }
+    if (clickCount == 9) {
+        alert("DRAW")
+        resetButton.style.display = "inline";
+    }
 
 }
